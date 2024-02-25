52802dfb32112a3b000dba75d3e0eeffcb75d887
Майстренко Мирослав Александрович <m.a.maystrenko@neovoxtech.ru>
2024-02-25 12:30:13 +0300

    REVIEW_TASK - with title etc

--- api_yamdb/review/models.py
+++ api_yamdb/review/models.py
@@ -1,14 +1,80 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
+
+from .validators import validate_year
 
 
-class CustomUser(AbstractUser):
-    pass
+class Category(models.Model):
+    """Категории произведений."""
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+        verbose_name='Слаг категории'
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'атегория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(models.Model):
+    """Жанры произведений."""
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Слаг жанра'
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
 
 
 class Title(models.Model):
-    pass
+    """Произведения."""
 
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название произведения'
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name='Год создания',
+        validators=[
+            MinValueValidator(0),
+            validate_year]
+    )
+    description = models.TextField(
+        verbose_name='Описание произведения',
+        blank=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreConnect',
+        related_name='genre',
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='category',
+        verbose_name='Категория',
+    )
 
 class Review(models.Model):
     text = models.TextField()
