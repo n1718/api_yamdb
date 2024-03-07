cb618c94cf44b23755af9f952d45e91cad2c201c
Майстренко Мирослав Александрович <m.a.maystrenko@neovoxtech.ru>
2024-03-07 19:50:02 +0300

    DEVELOPMENT_TASK_REVIEW flake8

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -2,9 +2,9 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CategoryViewSet, CommentViewSet,
-                          CustomUserViewSet, GenreViewSet,
-                          GetToken, ReviewViewSet,
-                          SignUp, TitleViewSet)
+                    CustomUserViewSet, GenreViewSet,
+                    GetToken, ReviewViewSet,
+                    SignUp, TitleViewSet)
 
 
 router_v1 = DefaultRouter()
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import (
-    MaxValueValidator, MinValueValidator, RegexValidator
+    MaxValueValidator, MinValueValidator
 )
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
